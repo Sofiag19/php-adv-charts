2e84a72c1019f12d93aefd6bbcdeab7c999c8995
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-12-12 15:02:31 +0100

    init

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Grafico Chart JS</title>
+  </head>
+  <body>
+    <div id="container">
+      <div id="wrapper">
+        <canvas id="myChart"></canvas>
+      </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js"></script>
+    <!-- -CDN 3.3.1 -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- -CDN GENERALE -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <!-- -CDN LINGUA ITALIANA -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/locale/it.js"></script>
+    <!-- script -->
+    <script src="script.js" charset="utf-8"></script>
+  </body>
+</html>
