38f64924b38728f58a2371d8f900610fef2c3966
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-12-13 13:05:49 +0100

    init step3

---
+++ step3/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style3.css">
+    <title>Advanced Dashboard Charts3</title>
+  </head>
+  <body>
+    <div id="container">
+      <div class="wrapper">
+        <canvas id="fatturato"></canvas>
+      </div>
+      <div class="wrapper">
+        <canvas id="fatturatoByAgent"></canvas>
+      </div>
+      <div class="wrapper">
+        <canvas id="teamEff"></canvas>
+      </div>
+    </div>
+
+    <!-- CDN Chart -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js"></script>
+    <!-- -CDN 3.3.1 -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- -CDN GENERALE -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <!-- -CDN LINGUA ITALIANA -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/locale/it.js"></script>
+    <!-- script -->
+    <script src="script.js" charset="utf-8"></script>
+  </body>
+</html>
